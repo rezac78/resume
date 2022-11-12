62d2d0592d4c5035ed0c35929e878fecce58838b
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-11-12 12:39:13 +0330

    create Part1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Profile from "./PortfolioContainer/Home/Profile"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Profile/>
+      </div>
+    );
+  }
+}
+
+export default App;
