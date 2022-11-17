147887390c8d15654c739ece395a73357789e9de
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-11-17 12:51:42 +0330

    end part navbar

--- src/PortfolioContainer/Home/Header/Header.jsx
+++ src/PortfolioContainer/Home/Header/Header.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Header.css'
-import Menu from "../../../assets/Home/menu.png"
+import MenuOpen from "../../../assets/Home/menu.png"
+import MenuClose from "../../../assets/Home/close.png"
 export default function Header() {
+        const [isOpen, setOpen] = useState(false)
+
+        const handleIsOpen = () => {
+                setOpen(!isOpen)
+                console.log(isOpen)
+        }
         return (
-                <div className="navbar">
-                        <div className="nav-links">
-                                <ul>
-                                        <li className="active"><a href="#" >Home</a></li>
-                                        <li><a href="#">AboutMe</a></li>
-                                        <li><a href="#">Resume</a></li>
-                                        <li><a href="#">ContactMe</a></li>
-                                </ul>
-                        </div>
-                        <img src={Menu} alt="menu hamburger" className="menu-hamburger"/>
+                <div isOpen={isOpen}
+                        onOpen={handleIsOpen}
+                        onClose={handleIsOpen} className="navbar">
+                        {
+                                isOpen === true ?
+                                        <div>
+                                                <div className="nav-links">
+                                                        <ul>
+                                                                <li className="active"><a href="#" >Home</a></li>
+                                                                <li><a href="#">AboutMe</a></li>
+                                                                <li><a href="#">Resume</a></li>
+                                                                <li><a href="#">ContactMe</a></li>
+                                                        </ul>
+                                                </div>
+                                                <div onClick={handleIsOpen}>
+                                                        <img src={MenuClose} alt="menu hamburger" className="menu-hamburger" />
+                                                </div>
+
+                                        </div>
+                                        :
+                                        <div>
+                                                <div className="nav-links-PC">
+                                                        <ul>
+                                                                <li className="active"><a href="#" >Home</a></li>
+                                                                <li><a href="#">AboutMe</a></li>
+                                                                <li><a href="#">Resume</a></li>
+                                                                <li><a href="#">ContactMe</a></li>
+                                                        </ul>
+                                                </div>
+                                                <div onClick={handleIsOpen}>
+                                                        <img src={MenuOpen} alt="menu hamburger" className="menu-hamburger" />
+                                                </div>
+                                        </div>
+                        }
                 </div>
         )
 }
