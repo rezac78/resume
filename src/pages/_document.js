97e938fad8db14ced72afe9fee0f6333672bbe9b
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-12-08 13:00:28 +0330

    ok

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="Resume online" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
@@ -15,7 +13,6 @@ class MyDocument extends Document {
             integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
             crossOrigin="anonymous"
           />
-          <title>RezaDalvand</title>
         </Head>
         <body>
           <Script
